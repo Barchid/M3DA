4795adb7036685ad3f64b1cfc8207d4076736917
Barchid Sami <barchid@b02p7.fil.univ-lille1.fr>
2019-09-18 13:28:14 +0200

    tp1

--- cylinder_Barchid/src/application/GLApplication.cpp
+++ cylinder_Barchid/src/application/GLApplication.cpp
@@ -31,119 +31,119 @@ EDraw _activeDraw;
 EPath _activePath;
 
 GLApplication::GLApplication() {
-  //
-  _leftPanelMenu << "Draw square (drawing example)" << "Input section (= square)" << "Input section (= circle)" << "Input section" << "Input Path" << "Switch Linear/Spline Path";
-  _leftPanelMenu  << "Draw extrusion" << "Draw revolution (Path = circle)";
-  _activeMenu=M_Draw_Square;
-  _activeDraw=D_Square;
-  _activePath=Path_Line;
+    //
+    _leftPanelMenu << "Draw square (drawing example)" << "Input section (= square)" << "Input section (= circle)" << "Input section" << "Input Path" << "Switch Linear/Spline Path";
+    _leftPanelMenu  << "Draw extrusion" << "Draw revolution (Path = circle)";
+    _activeMenu=M_Draw_Square;
+    _activeDraw=D_Square;
+    _activePath=Path_Line;
 
-  pathDefault();
-  sectionCircle();
+    pathDefault();
+    sectionCircle();
 
-  _cameraSection.ortho(-2.5,2.5,-2.5,2.5,0,2);
-  _cameraPath.ortho(-2.5,2.5,-2.5,2.5,0,2);
-  _cameraPath.position(0,1,0);
-  _cameraPath.lookAt(Vector3(0,0,0));
-  _cameraExtrusion.frustum(-_frustum,_frustum,-_frustum,_frustum,0.03,1000);
-  _cameraExtrusion.position(0,0,10);
-  _cameraExtrusion.lookAt(Vector3(0,0,0));
+    _cameraSection.ortho(-2.5,2.5,-2.5,2.5,0,2);
+    _cameraPath.ortho(-2.5,2.5,-2.5,2.5,0,2);
+    _cameraPath.position(0,1,0);
+    _cameraPath.lookAt(Vector3(0,0,0));
+    _cameraExtrusion.frustum(-_frustum,_frustum,-_frustum,_frustum,0.03,1000);
+    _cameraExtrusion.position(0,0,10);
+    _cameraExtrusion.lookAt(Vector3(0,0,0));
 
 }
 
 
 /** ********************************************************************** **/
 void GLApplication::initialize() {
-  // appelée 1 seule fois à l'initialisation du contexte
-  // => initialisations OpenGL
-  glClearColor(1,1,1,1);
+    // appelée 1 seule fois à l'initialisation du contexte
+    // => initialisations OpenGL
+    glClearColor(1,1,1,1);
 
-  glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
 
-  glEnable(GL_DEPTH_TEST);
-  glDepthFunc(GL_LESS);
-  glClearDepth(1);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    glClearDepth(1);
 
-  p3d::initGLTool();
+    p3d::initGLTool();
 
-  // ...
+    // ...
 }
 
 void GLApplication::resize(int width,int height) {
-  // appelée à chaque dimensionnement du widget OpenGL
-  // (inclus l'ouverture de la fenêtre)
-  // => réglages liés à la taille de la fenêtre
-  _cameraPath.viewport(0,0,width,height);
-  _cameraSection.viewport(0,0,width,height);
-  _cameraExtrusion.viewport(0,0,width,height);
-  // ...
+    // appelée à chaque dimensionnement du widget OpenGL
+    // (inclus l'ouverture de la fenêtre)
+    // => réglages liés à la taille de la fenêtre
+    _cameraPath.viewport(0,0,width,height);
+    _cameraSection.viewport(0,0,width,height);
+    _cameraExtrusion.viewport(0,0,width,height);
+    // ...
 }
 
 void GLApplication::update() {
-  // appelée toutes les 20ms (60Hz)
-  // => mettre à jour les données de l'application
-  // avant l'affichage de la prochaine image (animation)
-  // ...
+    // appelée toutes les 20ms (60Hz)
+    // => mettre à jour les données de l'application
+    // avant l'affichage de la prochaine image (animation)
+    // ...
 
-  if (_activeDraw==D_Path) {
-    if (mouseLeftPressed()) {
-      _path.push_back(_cameraPath.windowToWorld(mouseX(),mouseY()));
+    if (_activeDraw==D_Path) {
+        if (mouseLeftPressed()) {
+            _path.push_back(_cameraPath.windowToWorld(mouseX(),mouseY()));
+        }
+        if (keyPressed(Qt::Key_X)) _path.clear();
     }
-    if (keyPressed(Qt::Key_X)) _path.clear();
-  }
-  if (_activeDraw==D_Section) {
-    if (mouseLeft()) {
-      _section.push_back(_cameraSection.windowToWorld(mouseX(),mouseY()).xy());
+    if (_activeDraw==D_Section) {
+        if (mouseLeft()) {
+            _section.push_back(_cameraSection.windowToWorld(mouseX(),mouseY()).xy());
+        }
+        if (keyPressed(Qt::Key_X)) _section.clear();
     }
-    if (keyPressed(Qt::Key_X)) _section.clear();
-  }
-  if (_activeDraw==D_Extrusion) {
-    updateCameraExtrusion();
-  }
+    if (_activeDraw==D_Extrusion) {
+        updateCameraExtrusion();
+    }
 }
 
 /* ************************************************************ */
 
 void GLApplication::updateCameraExtrusion() {
-  if (mouseLeft()) {
-    Vector3 center=_cameraExtrusion.pointTo(Coordinate_Local,Vector3(0,0,0));
-    Vector3 vertical=Vector3(0,1,0);
-    _cameraExtrusion.translate(center,Coordinate_Local);
-    _cameraExtrusion.rotate(-deltaMouseX()/2.0,vertical,Coordinate_Local);
-    _cameraExtrusion.rotate(deltaMouseY()/2.0,Vector3(1,0,0),Coordinate_Local);
-    _cameraExtrusion.translate(-center,Coordinate_Local);
-  }
-  if (left()) _cameraExtrusion.left(0.3);
-  if (right()) _cameraExtrusion.right(0.3);
-  if (forward()) _cameraExtrusion.forward(0.3);
-  if (backward()) _cameraExtrusion.backward(0.3);
-  if (accelerateWheel()) {
-    _frustum*=1.05;
-    _cameraExtrusion.frustum(-_frustum,_frustum,-_frustum,_frustum,0.03,1000);
-  }
-  if (decelerateWheel()) {
-    _frustum/=1.05;
-    _cameraExtrusion.frustum(-_frustum,_frustum,-_frustum,_frustum,0.03,1000);
-  }
+    if (mouseLeft()) {
+        Vector3 center=_cameraExtrusion.pointTo(Coordinate_Local,Vector3(0,0,0));
+        Vector3 vertical=Vector3(0,1,0);
+        _cameraExtrusion.translate(center,Coordinate_Local);
+        _cameraExtrusion.rotate(-deltaMouseX()/2.0,vertical,Coordinate_Local);
+        _cameraExtrusion.rotate(deltaMouseY()/2.0,Vector3(1,0,0),Coordinate_Local);
+        _cameraExtrusion.translate(-center,Coordinate_Local);
+    }
+    if (left()) _cameraExtrusion.left(0.3);
+    if (right()) _cameraExtrusion.right(0.3);
+    if (forward()) _cameraExtrusion.forward(0.3);
+    if (backward()) _cameraExtrusion.backward(0.3);
+    if (accelerateWheel()) {
+        _frustum*=1.05;
+        _cameraExtrusion.frustum(-_frustum,_frustum,-_frustum,_frustum,0.03,1000);
+    }
+    if (decelerateWheel()) {
+        _frustum/=1.05;
+        _cameraExtrusion.frustum(-_frustum,_frustum,-_frustum,_frustum,0.03,1000);
+    }
 }
 
 
 void GLApplication::draw() {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  switch(_activeDraw) {
-  case D_Square: p3d::apply(_cameraSection);drawSquare();break;
-  case D_Section: p3d::apply(_cameraSection);drawSection();break;
-  case D_Extrusion: p3d::apply(_cameraExtrusion);drawExtrusion();break;
-  case D_Path:
-    apply(_cameraPath);    if (_activePath==Path_Line) {
-      drawPathLine();
+    switch(_activeDraw) {
+    case D_Square: p3d::apply(_cameraSection);drawSquare();break;
+    case D_Section: p3d::apply(_cameraSection);drawSection();break;
+    case D_Extrusion: p3d::apply(_cameraExtrusion);drawExtrusion();break;
+    case D_Path:
+        apply(_cameraPath);    if (_activePath==Path_Line) {
+            drawPathLine();
+        }
+        else if (_activePath==Path_Spline) {
+            drawPathSpline();
+        }
+    default:break;
     }
-    else if (_activePath==Path_Spline) {
-      drawPathSpline();
-    }
-  default:break;
-  }
 }
 
 
@@ -157,29 +157,29 @@ void GLApplication::drawSquare() {
 
 
 
-  // compute points of the square in array of Vector2
-  vector<Vector2> pts;
-  pts.resize(5);
-  pts[0]=Vector2(-0.5,-0.5);
-  pts[1]=Vector2(0.5,-0.5);
-  pts[2]=Vector2(0.5,0.5);
-  pts[3]=Vector2(-0.5,0.5);
-  pts[4]=pts[0];
+    // compute points of the square in array of Vector2
+    vector<Vector2> pts;
+    pts.resize(5);
+    pts[0]=Vector2(-0.5,-0.5);
+    pts[1]=Vector2(0.5,-0.5);
+    pts[2]=Vector2(0.5,0.5);
+    pts[3]=Vector2(-0.5,0.5);
+    pts[4]=pts[0];
 
-  // draw square (line strip)
-  p3d::ambientColor=Vector4(1,0,0,1); // the drawing color for the shader : must be set before the call p3d::shaderVertexAmbient
-  p3d::shaderVertexAmbient(); // enable a simple shader.
-  p3d::drawLineStrip(pts,5); // draw the array of Vector2D with the actual shader (you must set the number of points to draw : 5 here for the square).
+    // draw square (line strip)
+    p3d::ambientColor=Vector4(1,0,0,1); // the drawing color for the shader : must be set before the call p3d::shaderVertexAmbient
+    p3d::shaderVertexAmbient(); // enable a simple shader.
+    p3d::drawLineStrip(pts,5); // draw the array of Vector2D with the actual shader (you must set the number of points to draw : 5 here for the square).
 
-  // draw vertices (points)
-  glPointSize(10);
-  p3d::ambientColor=Vector4(0,0,1,1);
-  p3d::shaderVertexAmbient();
-  p3d::drawPoints(pts);
+    // draw vertices (points)
+    glPointSize(10);
+    p3d::ambientColor=Vector4(0,0,1,1);
+    p3d::shaderVertexAmbient();
+    p3d::drawPoints(pts);
 
-  // draw text
-  p3d::ambientColor=Vector4(1,0,1,1);
-  p3d::draw("V0",Vector3(pts[0],0));
+    // draw text
+    p3d::ambientColor=Vector4(1,0,1,1);
+    p3d::draw("V0",Vector3(pts[0],0));
 
 
 }
@@ -193,12 +193,12 @@ void GLApplication::drawSquare() {
  */
 
 void GLApplication::sectionSquare() {
-  _section.clear();
-  _section.push_back(Vector2(-1,-1));
-  _section.push_back(Vector2(1,-1));
-  _section.push_back(Vector2(1,1));
-  _section.push_back(Vector2(-1,1));
-  _section.push_back(Vector2(-1,-1));
+    _section.clear();
+    _section.push_back(Vector2(-1,-1));
+    _section.push_back(Vector2(1,-1));
+    _section.push_back(Vector2(1,1));
+    _section.push_back(Vector2(-1,1));
+    _section.push_back(Vector2(-1,-1));
 }
 
 
@@ -207,21 +207,35 @@ void GLApplication::sectionSquare() {
  * set the cross section (i.e. set _inputCrossSection) as a circle
  */
 void GLApplication::sectionCircle() {
-  _section.clear();
+    _section.clear();
+    // En utilisant la théorie ici https://www.mathopenref.com/coordparamcircle.html
+    float nbPoints = 25; // nombre de points voulus pour tracer le cercle
+    float x,y; // coordonnées des points à trouver
+    float t = 0; // angle du point à trouver par rapport au centre du cercle
 
+    for(int i = 0; i < nbPoints; i++) {
+        // application de la formule
+        x = cos(t);
+        y = sin(t);
+        _section.push_back(Vector2(x,y));
 
+        // mise à jour de l'angle pour le prochain point
+        t += (2.0*M_PI) /(nbPoints-1.0);
+    }
+    //_section.push_back(Vector2(x,y));
 }
 
 
 /** ************************************************************************ **/
 
 void GLApplication::pathDefault() {
-
-  _path.clear();
-  _path.push_back(Vector3(0,0,-2));
-  _path.push_back(Vector3(0,0,2));
-
-
+    // Q7 : commenter et mettre l'autre segment pour que ce soit non orthogonal au plan XY
+    //    _path.clear();
+    //    _path.push_back(Vector3(0,0,-2));
+    //    _path.push_back(Vector3(0,0,2));
+    _path.clear();
+    _path.push_back(Vector3(-2,0,-2));
+    _path.push_back(Vector3(2,0,2));
 }
 
 void GLApplication::pathCircle() {
@@ -232,33 +246,33 @@ void GLApplication::pathCircle() {
 /** ************************************************************************ **/
 
 void GLApplication::drawSection() {
-  p3d::ambientColor=Vector4(1,0,0,1);
-  p3d::shaderVertexAmbient();
-  p3d::drawPoints(_section);
-  p3d::drawLineStrip(_section);
+    p3d::ambientColor=Vector4(1,0,0,1);
+    p3d::shaderVertexAmbient();
+    p3d::drawPoints(_section);
+    p3d::drawLineStrip(_section);
 }
 
 void GLApplication::drawPathLine() {
-  if (_path.size()<1) return;
+    if (_path.size()<1) return;
 
-  p3d::ambientColor=Vector4(0,0,1,1);
-  p3d::shaderVertexAmbient();
-  p3d::drawPoints(_path);
-  p3d::drawLineStrip(_path);
+    p3d::ambientColor=Vector4(0,0,1,1);
+    p3d::shaderVertexAmbient();
+    p3d::drawPoints(_path);
+    p3d::drawLineStrip(_path);
 }
 
 
 void GLApplication::drawExtrusion() {
-  if (_extrusion.size()<4) return;
-  int nbSlice=_section.size();
+    if (_extrusion.size()<4) return;
+    int nbSlice=_section.size();
 
-  p3d::ambientColor=Vector4(1,0,0,1);
-  p3d::shaderVertexAmbient();
+    p3d::ambientColor=Vector4(1,0,0,1);
+    p3d::shaderVertexAmbient();
 
 
-  drawGrid(_extrusion,nbSlice); // comment this once last question done
+    drawGrid(_extrusion,nbSlice); // comment this once last question done
 
-  /*
+    /*
    *  uncomment once normals computed (last question)
   p3d::lightPosition[0]=Vector4(0,0,10,1);
   p3d::lightIntensity[0]=1.0;
@@ -270,33 +284,33 @@ void GLApplication::drawExtrusion() {
 
 
 
-  drawPath();
+    drawPath();
 
 }
 
 
 
 void GLApplication::drawPathSpline() {
-  if (_path.size()>=2) {
-    vector<Vector3> toDraw;
-    toDraw.clear();
-    int nbPts=100;
-    double step=1.0/(nbPts-1);
-    double t=0;
-    for(int i=0;i<nbPts;++i) {
-      toDraw.push_back(pointSpline(t));
-      t+=step;
-    }
+    if (_path.size()>=2) {
+        vector<Vector3> toDraw;
+        toDraw.clear();
+        int nbPts=100;
+        double step=1.0/(nbPts-1);
+        double t=0;
+        for(int i=0;i<nbPts;++i) {
+            toDraw.push_back(pointSpline(t));
+            t+=step;
+        }
 
-    p3d::ambientColor=Vector4(0,0,1,1);
-    p3d::shaderVertexAmbient();
-    drawLineStrip(toDraw,toDraw.size());
-  }
-  if (_path.size()>0) {
-    p3d::ambientColor=Vector4(0,0,1,1);
-    p3d::shaderVertexAmbient();
-    p3d::drawPoints(_path);
-  }
+        p3d::ambientColor=Vector4(0,0,1,1);
+        p3d::shaderVertexAmbient();
+        drawLineStrip(toDraw,toDraw.size());
+    }
+    if (_path.size()>0) {
+        p3d::ambientColor=Vector4(0,0,1,1);
+        p3d::shaderVertexAmbient();
+        p3d::drawPoints(_path);
+    }
 }
 
 /** ************************************************************************* **/
@@ -309,60 +323,70 @@ void GLApplication::drawPathSpline() {
  */
 
 Vector3 GLApplication::rotatePlane(const Vector3 &p,const Vector3 &n) {
-  Vector3 result;
+    Vector3 result;
 
-  return result;
+    return result;
 }
 
 
 
 Vector3 GLApplication::pointSpline(double tNormalized) {
-  Vector3 result;
+    Vector3 result;
 
-  return result;
+    return result;
 }
 
 
 Vector3 GLApplication::tangentPathSpline(double tNormalized) {
-  Vector3 result;
+    Vector3 result;
 
-  return result;
+    return result;
 }
 
 
 
 Vector3 GLApplication::tangentPathLine(unsigned int i) {
-  Vector3 result;
+    Vector3 result;
 
 
-  return result;
+    return result;
 }
 
 /** ************************************************************************* **/
 
 void GLApplication::normalSection() {
-  _normalSection.clear();
+    _normalSection.clear();
 
 
 }
 
 
 void GLApplication::extrudeLine() {
-  if (_path.size()<1 || _section.size()<1) return;
+    if (_path.size()<1 || _section.size()<1) return;
 
-  _extrusion.clear();
-  _normalExtrusion.clear(); // for lighting (last question)
+    _extrusion.clear();
+    _normalExtrusion.clear(); // for lighting (last question)
 
+    float x; // coordonnées X de section
+    float y; // coordonnées Y de section
+    float z; // coordonnées Z de path
 
-
-
+    for(Vector3 ptPath : _path) {
+        z = ptPath.z();
+        for(Vector2 ptSection : _section) {
+            x = ptSection.x();
+            y = ptSection.y();
+            Vector3 ptExtrusion = Vector3(x,y,z);
+            _extrusion.push_back(ptExtrusion);
+        }
+    }
 }
 
 void GLApplication::extrudeSpline() {
-  if (_path.size()<1 || _section.size()<1) return;
+    if (_path.size()<1 || _section.size()<1) return;
 
-  _extrusion.clear();
-  _normalExtrusion.clear(); // for lighting (last question)
+    _extrusion.clear();
+    _normalExtrusion.clear(); // for lighting (last question)
 
 
 }
@@ -372,25 +396,25 @@ void GLApplication::extrudeSpline() {
 
 
 double GLApplication::scale(double tNormalized) {
-  return 1.0;
+    return 1.0;
 
 }
 
 void GLApplication::buildExtrusion() {
-  if (_activePath==Path_Line)
-    extrudeLine();
-  else if (_activePath==Path_Spline) {
-    extrudeSpline();
-  }
+    if (_activePath==Path_Line)
+        extrudeLine();
+    else if (_activePath==Path_Spline) {
+        extrudeSpline();
+    }
 }
 
 void GLApplication::drawPath() {
-  if (_activePath==Path_Line) {
-    drawPathLine();
-  }
-  else if (_activePath==Path_Spline) {
-    drawPathSpline();
-  }
+    if (_activePath==Path_Line) {
+        drawPathLine();
+    }
+    else if (_activePath==Path_Spline) {
+        drawPathSpline();
+    }
 }
 
 
@@ -398,46 +422,46 @@ void GLApplication::drawPath() {
 /** i = button number, s = button text
  */
 void GLApplication::leftPanel(int i,const std::string &) {
-  _activeMenu=static_cast<EMenu>(i);
+    _activeMenu=static_cast<EMenu>(i);
 
-  switch (_activeMenu) {
-  case M_Draw_Square:
-    _activeDraw=D_Square;
-    break;
-  case M_Set_Section_Square:
-    sectionSquare();
-    _activeDraw=D_Section;
-    break;
-  case M_Set_Section_Circle:
-    sectionCircle();
-    _activeDraw=D_Section;
-    break;
-  case M_Build_Extrusion:
-    buildExtrusion();
-    _activeDraw=D_Extrusion;
-    break;
-  case M_Build_Revolution:
-    pathCircle();
-    buildExtrusion();
-    _activeDraw=D_Extrusion;
-    break;
-  case M_Draw_Path:
-    _activeDraw=D_Path;
-    break;
-  case M_Draw_Section:
-    _activeDraw=D_Section;
-    break;
-  case M_Spline_Line:
-    if (_activePath==Path_Line) _activePath=Path_Spline;
-    else _activePath=Path_Line;
-    if (_activeDraw==D_Extrusion) {
-      buildExtrusion();
+    switch (_activeMenu) {
+    case M_Draw_Square:
+        _activeDraw=D_Square;
+        break;
+    case M_Set_Section_Square:
+        sectionSquare();
+        _activeDraw=D_Section;
+        break;
+    case M_Set_Section_Circle:
+        sectionCircle();
+        _activeDraw=D_Section;
+        break;
+    case M_Build_Extrusion:
+        buildExtrusion();
+        _activeDraw=D_Extrusion;
+        break;
+    case M_Build_Revolution:
+        pathCircle();
+        buildExtrusion();
+        _activeDraw=D_Extrusion;
+        break;
+    case M_Draw_Path:
+        _activeDraw=D_Path;
+        break;
+    case M_Draw_Section:
+        _activeDraw=D_Section;
+        break;
+    case M_Spline_Line:
+        if (_activePath==Path_Line) _activePath=Path_Spline;
+        else _activePath=Path_Line;
+        if (_activeDraw==D_Extrusion) {
+            buildExtrusion();
+        }
+        break;
+    default:break;
     }
-    break;
-  default:break;
-  }
 
-  /*
+    /*
   switch (i) {
     case 0:...;break;
     case 1:...;break;
